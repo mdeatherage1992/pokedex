834e55bf74f3bf00fa67c44ddeaada89ad711a4f
Matthew Deatherage <mdeatherage1992@gmail.com>
2019-10-04 08:54:57 -0400

    added moves to show

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -2,6 +2,12 @@ class PokemonsController < ApplicationController
 
 def show
   @poke = Pokemon.find(params[:id])
+  @types = PokemonType.where(pokemon_id: @poke.id)
+  @moves = []
+  @types.each do |x|
+    r = Move.where(poke_type_id: x.poke_type_id)
+    r.each {|y| @moves.push(y)}
+  end
 end
 
 end
